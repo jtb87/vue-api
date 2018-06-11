748d9acde995095bfb9cbdb8327a2223332ce66b
Jesse Burger <jesse.burger@sungevity-intl.com>
2018-06-11 17:18:11 +0200

    playing around with cookies && vue expressions and directives

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,11 +1,16 @@
 <template>
   <div id=API>
+    <section v-if="cookie.loggedin != 'true'">
     <form @submit.prevent="HandleForm">
       <input type="text" v-model="user" placeholder="username">
       <input type="text" v-model="password" placeholder="password">
       <button type="submit">Click</button>
-	</form>
- <p> {{ formdata }} </p>
+	 </form>
+  </section>
+  <p> {{ formdata }} </p>
+  <p> {{ cookie.username }} </p>
+  <p> {{ cookie }} </p>
+  <button @click="removeCookie">removeCookie</button>
   </div>
 </template>
 
@@ -17,14 +22,34 @@ export default {
       formdata: "data",
       password: "",
       user: "",
+      cookie: this.getCookie(),
+
     }
     },
+    // created(){
+    //   this.testuser = this.getCookie()
+
+    // },
     methods: {
       HandleForm: function(){
         this.formdata = this.password + " - " + this.user
+        document.cookie = "username=" + this.user + ";"
+        document.cookie = "loggedin=" + true + ";"
+      },
+      getCookie: function(){
+        let cookie = {};
+        document.cookie.split(';').forEach(function(el) {
+          let [k,v] = el.split('=');
+          cookie[k.trim()] = v;
+      })
+        return cookie;
+      }, 
+      removeCookie: function(){
+        document.cookie = "loggedin=" + false + ";"
+      }
+
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
