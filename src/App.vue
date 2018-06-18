de5842721fd419c952f0c9e37c50f66ee20fb7cf
Jesse Burger <jesse.burger@sungevity-intl.com>
2018-06-18 15:11:09 +0200

    played around with bundling a package

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
   <div id="app">
-    <div class="md-layout">
+   <!--  <div class="md-layout">
       <div class="md-layout-item"></div>
-      <div class="md-layout-item">
+      <div class="md-layout-item"> -->
         <!-- <img src="./assets/logo2.png"> -->
-          <HelloWorld msg="Lambda Go/Vue project"/>
+          <CardComponent />
           <!-- <ApiComponent /> -->
-      </div>
+      <!-- </div>
       <div class="md-layout-item"></div>
     </div>
-   
+    -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import CardComponent from './components/CardComponent.vue'
 import ApiComponent from './components/ApiComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     CardComponent, 
-    ApiComponent
+    ApiComponent, 
   }
 }
+
 </script>
 
 <style>
 
 #app {
+  max-width:600px;
   font-family: 'Roboto','Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-
-
-import { MdCard,MdButton, MdField } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-
+import { MdCard,MdButton, MdField } from 'vue-material/dist/components'
 
 Vue.use(MdCard)
 Vue.use(MdButton)
 Vue.use(MdField)
+
 
 Vue.config.productionTip = false
 
